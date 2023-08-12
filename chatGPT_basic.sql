d1042e11e024a73aac3ae7b2acb4e7b70beac9be
Jess Yang <jessyang@Jesss-Mac-mini.local>
2023-08-12 15:24:48 -0700

    init file

---
+++ chatGPT_basic.sql
@@ -0,0 +1,7 @@
+-- SELECT: Used to retrieve data from one or more tables.
+-- FROM: Specifies the table(s) you're querying.
+SELECT column1, column2 FROM table_name;
+
+-- WHERE: Filters data based on a condition.
+SELECT * FROM customers WHERE age > 24;
+
